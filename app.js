ac24ef4519c6153d4fd26576fae1ca0abccbab7d
hyohyo814 <sojaisam@gmail.com>
2023-07-08 16:31:10 -0700

    Missed quotes and allow console log pass

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-const express = require("express")
+const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
@@ -8,5 +8,6 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
+  //eslint-disable-next-line
   console.log('server started on port 5000')
 })
