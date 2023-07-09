af450d82ab734566fa64fbb7acfdf1420efaea3e
hyohyo814 <sojaisam@gmail.com>
2023-07-09 15:10:46 -0700

    Add http checks

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+//eslint-disable-next-line
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   //eslint-disable-next-line
   console.log('server started on port 5000')
