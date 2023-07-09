26e39ee80df3d895c2f6c79e2931a5f0bc763cc8
hyohyo814 <sojaisam@gmail.com>
2023-07-08 17:46:01 -0700

    Remove unnecessary function

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,6 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(_on, _config) {
-      // implement node event listeners here
-    },
     baseUrl: 'http://localhost:5000',
   },
 })
