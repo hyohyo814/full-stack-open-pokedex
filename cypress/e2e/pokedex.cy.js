1dc5374e48e78ea26c448e8ced17943ad165ba63
hyohyo814 <sojaisam@gmail.com>
2023-07-08 23:25:27 -0700

    Additional test for navigating a singular pokemon

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,7 +1,21 @@
-describe('Pokedex', function() {
-  it('front page can be opened', function() {
+describe('Pokedex', function () {
+  it('front page can be opened', function () {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+    cy.contains(
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    )
+  })
+
+  it('navigate to wartortle', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('wartortle').click()
+    cy.get('.pokemon-image').should(
+      'have.css',
+      'background-image',
+      'url("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png")'
+    )
+    cy.contains('torrent')
+    cy.contains('rain dish')
   })
 })
